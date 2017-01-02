4b60300e35a3e301b59859520131f4d16a372cc9
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-01-02 21:05:00 +0100

    s2: twrp: set default brightness, blacklist mouse event object
    Change-Id: I09db04637889f98d28994ec604867e936a105da3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,9 +187,12 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := "160"
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
+#TWRP_EVENT_LOGGING := true
 else
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 endif
