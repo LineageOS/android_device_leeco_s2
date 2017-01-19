5539814ee409ca17470f6946c2503915063f74a8
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-01-19 23:22:12 +0100

    s2: fs config: add cnss
    Change-Id: Icefd99ba8d86e8c54e2eab56e94ba751ad461d1a

--- android_filesystem_config.h
+++ android_filesystem_config.h
@@ -35,6 +35,7 @@
       { 00700, AID_RADIO,          AID_SHELL,          (1ULL << CAP_BLOCK_SUSPEND), "system/bin/qmuxd" },
       { 00700, AID_CAMERA,         AID_SHELL,          (1ULL << CAP_SYS_NICE), "system/bin/mm-qcamera-daemon" },
       { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/pm-service"},
+      { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/cnss-daemon"},
       { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/imsdatadaemon" },
       { 00755, AID_SYSTEM,         AID_RADIO,          (1ULL << CAP_NET_BIND_SERVICE), "system/bin/ims_rtp_daemon" },
 };
