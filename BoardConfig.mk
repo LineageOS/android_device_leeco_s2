66c76274d2e99d6aa9116c15d9b428777609a0fb
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-02-18 08:49:59 +0100

    s2: enable dexpreopt, clang platform build
    Change-Id: I3c696c609269f237e002c001cb0f5ce3aeec41c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,6 +188,15 @@ TARGET_USES_WCNSS_MAC_ADDR_REV := true
 BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
 
+# Enable dexpreopt to speed boot time
+ifeq ($(HOST_OS),linux)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
 # Twrp
 #RECOVERY_VARIANT := twrp
 ifeq ($(RECOVERY_VARIANT),twrp)
@@ -205,6 +214,7 @@ TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 #TWRP_EVENT_LOGGING := true
 else
+USE_CLANG_PLATFORM_BUILD := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 endif
 
