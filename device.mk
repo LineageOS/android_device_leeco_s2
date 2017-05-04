375412ca09e8455e3b8a382b98e8d5221a0bce2b
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    s2: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: I0fbb03603a72d0108a96bfa0f0a4ceb12ef3d430

--- device.mk
+++ device.mk
@@ -243,8 +243,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles_8956.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
