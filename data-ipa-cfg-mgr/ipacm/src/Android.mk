283ef1b6ac59c8173b9bc9f00a8f68e32118e96e
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-05-26 21:37:30 +0200

    s2: ipa: update to LA.BR.1.3.6-03910-8976.0
    Change-Id: I0d188f05bf03d670567bea23fb0ab763215b074c

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -1,3 +1,9 @@
+BOARD_PLATFORM_LIST := msm8916
+BOARD_PLATFORM_LIST += msm8909
+ifneq ($(call is-board-platform-in-list,$(BOARD_PLATFORM_LIST)),true)
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+ifneq (, $(filter aarch64 arm arm64, $(TARGET_ARCH)))
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -62,6 +68,7 @@ LOCAL_SHARED_LIBRARIES := libipanat
 LOCAL_SHARED_LIBRARIES += libxml2
 LOCAL_SHARED_LIBRARIES += libnfnetlink
 LOCAL_SHARED_LIBRARIES += libnetfilter_conntrack
+LOCAL_SHARED_LIBRARIES += libdhcpcd
 LOCAL_CLANG := true
 include $(BUILD_EXECUTABLE)
 
@@ -87,3 +94,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_OWNER := ipacm
 include $(BUILD_PREBUILT)
+
+endif # $(TARGET_ARCH)
+endif
+endif
