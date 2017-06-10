beadc526b7a8d86f40a1b99be23c4c9945396838
Marios Thomos <marios199546@gmail.com>
2017-06-10 11:37:40 +0300

    Adding audio flags and enabling QCT resampler
    Change-Id: I086c48769e3ff5d453d1487ef2abf3f330c1d8d8

---
+++ include/QCT_Resampler.h
@@ -0,0 +1,29 @@
+#ifndef ANDROID_QCT_RESAMPLER_API_H
+#define ANDROID_QCT_RESAMPLER_API_H
+/*============================================================================
+ Copyright (c) 2012 Qualcomm Technologies, Inc.
+ All Rights Reserved. Qualcomm Technologies Proprietary and Confidential.
+============================================================================*/
+
+#include <stdint.h>
+#include <sys/types.h>
+
+namespace android {
+
+class QCT_Resampler {
+
+public:
+	static void		Init(int16_t *pState, int32_t inChannelCount, int32_t inSampleRate, int32_t mSampleRate, int32_t) {
+		Init(pState, inChannelCount, inSampleRate, mSampleRate);
+	}
+	static void		Resample90dB(int16_t* pState, int32_t* in, int32_t* out, size_t inFrameCount, size_t outFrameCount) {
+		Resample90dB(pState, (int16_t*)in, out, inFrameCount, outFrameCount);
+	}
+
+	static size_t	MemAlloc(int bitDepth, int inChannelCount, int32_t inSampleRate, int32_t sampleRate);
+	static void		Init(int16_t *pState, int32_t inChannelCount, int32_t inSampleRate, int32_t mSampleRate);
+	static void		Resample90dB(int16_t* pState, int16_t* in, int32_t* out, size_t inFrameCount, size_t outFrameCount);
+	static size_t	GetNumInSamp(int16_t* pState, size_t outFrameCount);
+};
+};
+#endif  //ANDROID_QCT_RESAMPLER_API_H
