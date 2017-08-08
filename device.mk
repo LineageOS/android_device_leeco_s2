c995bee9c8ab9708ede6c2d9ba4acf490add9b4f
Marios Thomos <marios199546@gmail.com>
2017-08-08 11:21:02 +0000

    s2: Introduce support for QPerfomance
    -Import QPerfomance files from 5.9.0.26S
    -Import QPerfomance configuration from Helium/Hydrogen
    
    
    Change-Id: I639116e1f4410a8b4a6aa8f2529e6c326cd0ae48

--- device.mk
+++ device.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.qcom
 
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir,root)
