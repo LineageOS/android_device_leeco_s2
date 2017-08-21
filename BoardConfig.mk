ed15418d1622f2a18625cf703c84fa02db5ce304
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-21 12:40:08 +0200

    s2: enable hacks for legacy camera hal
    Change-Id: Idb798f5c3393c17989a14fa13d90b9ce4d89bf9f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ QCOM_BT_USE_SMD_TTY := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_QTI_CAMERA_32BIT_ONLY := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
 # This is needed for us as it disables tcache, which is breaking camera.
 MALLOC_SVELTE := true
