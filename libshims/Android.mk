e5ea461dd8cf4eeee5c8951404003e05fde7a495
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-22 11:12:17 +0200

    s2: camera: import LA.BR.1.3.6_rb1.18 from caf, add cameraservice shim
    Change-Id: I5577c21d0964af4c47a05e020fd68300007244f5

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# libshims_ims
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -26,6 +28,8 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
+
+# libshims_camera
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -38,5 +42,37 @@ LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+# libshims_cameraservice
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    libcameraservice/CameraFlashlight.cpp
+
+LOCAL_C_INCLUDES += \
+    frameworks/native/include/media/openmax
+
+LOCAL_SHARED_LIBRARIES:= \
+    libui \
+    liblog \
+    libutils \
+    libbinder \
+    libcutils \
+    libmedia \
+    libmediautils \
+    libcamera_client \
+    libgui \
+    libhardware \
+    libsync \
+    libcamera_metadata \
+    libjpeg \
+    libmemunreachable \
+    libcameraservice
+
+LOCAL_MODULE := libshims_cameraservice
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
