7ea15a62fc1bfa8562e95cdf10f8d704ad9ceae3
Demon000 <demonsingur@gmail.com>
2017-08-31 17:25:21 +0300

    gps: remove slim_daemon
    It's a PREMIUM_FEATURE daemon and it's causing errors on O.
    
    01-29 01:03:28.327   877   877 F DEBUG   : Abort message: 'CANNOT LINK EXECUTABLE "slim_daemon": cannot locate symbol "_ZN7android16SensorEventQueue4readEP12ASensorEventm" referenced by "/system/vendor/bin/slim_daemon"...'
    
    Change-Id: Iba1342d12b10e94be9e37724795a922996d32773

--- config.fs
+++ config.fs
@@ -34,12 +34,6 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[vendor/bin/slim_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
 [system/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
