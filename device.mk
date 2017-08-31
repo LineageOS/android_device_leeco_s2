1833eb36821c9c816b77e0a3c7777c8150d425a7
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-31 13:48:55 +0200

    s2: camera: use proprietary hal
    open source hal sometimes failes to get camera caps
    
    Change-Id: Ifd9e929a13528c9a0b09cb2d5f823a961fa89a71

--- device.mk
+++ device.mk
@@ -142,8 +142,11 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8952 \
-    Snap
+    Snap \
+    libqomx_core \
+    libmmcamera_interface \
+    libmmjpeg_interface
+#    camera.msm8952.so \
 
 # Charger
 PRODUCT_PACKAGES += \
