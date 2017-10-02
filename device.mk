132fe9d4e3533484f217d1117258d431a45d53ee
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-02 07:55:25 +0200

    s2: fix heaphone button mapping
    Source: https://forum.xda-developers.com/showpost.php?p=74004025&postcount=3332
    
    Thanks to BigFatTony@XDA
    
    Change-Id: I9a8c11d14f6c2263bb98fb1537276a2bd14d3b12

--- device.mk
+++ device.mk
@@ -333,6 +333,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/msm8976-skun-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-skun-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
     $(LOCAL_PATH)/keylayout/synaptics_dsx_s2.kl:system/usr/keylayout/synaptics_dsx_s2.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
