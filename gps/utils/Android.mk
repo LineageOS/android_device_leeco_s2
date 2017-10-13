5b9d4442505d7fdfcccb8310ae591dc800799a2a
Vishnu Das V R <vishnudasvr07@gmail.com>
2017-10-13 20:09:22 +0530

    s2: gps: add liblog dependency

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -9,7 +9,8 @@ include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
-    liblog
+    liblog \
+    libsensor
 
 LOCAL_SRC_FILES += \
     loc_log.cpp \
@@ -22,7 +23,8 @@ LOCAL_SRC_FILES += \
     LocTimer.cpp \
     LocThread.cpp \
     MsgTask.cpp \
-    loc_misc_utils.cpp
+    loc_misc_utils.cpp \
+    process_name.c
 
 # Flag -std=c++11 is not accepted by compiler when LOCAL_CLANG is set to true
 LOCAL_CFLAGS += \
@@ -37,7 +39,8 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
 LOCAL_C_INCLUDES:= \
-    $(LOCAL_PATH)/platform_lib_abstractions
+    $(LOCAL_PATH)/platform_lib_abstractions \
+    framework/native/libs/sensor/include
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
---
+++ gps/utils/process_name.c
@@ -0,0 +1,5 @@
+static const char* process_name = "unknown";
+
+const char* get_process_name(void) {
+    return process_name;
+}
