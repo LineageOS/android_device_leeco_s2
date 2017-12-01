faa92f12c66b868513359bb7f2b29fc620a93332
TheScarastic <warabhishek@gmail.com>
2017-12-01 01:03:01 -0600

    s2: add custom biometrics to fix gdxfingerprint
     * Avoid notifying cancel hacks like https://github.com/AOSPA/android_framework$
       as they will conflict with the implementation in the biometricservice
    
     * Co-authored with @alexstsv2
    
    Change-Id: Ieb7b8843d2fa5f1699b432ac529fb656c608467d
    Signed-off-by: TheScarastic <warabhishek@gmail.com>

--- fingerprint/wrapper/Android.mk
+++ fingerprint/wrapper/Android.mk
@@ -16,4 +16,16 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    FingerprintWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := fingerprint.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
