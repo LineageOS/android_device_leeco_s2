36beb8c4681b2f59188bdadb6ab1b0e4d01605d8
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    s2: Switch to Soundtrigger 2.1 implementation implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: Ib53fc0de3acd5147c0cfa5e09bfadd55c4361b02

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -20,8 +20,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service
 
 # Camera
 PRODUCT_PACKAGES += \
