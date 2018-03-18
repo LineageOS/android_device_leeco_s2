66558582f2d03210c876a13106b290b7a896d2e4
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-18 12:44:45 +0100

    s2: trash additional default properties
    Change-Id: I2c64a5e6da3df551ac755c9aaf694bcbdb831724

--- device.mk
+++ device.mk
@@ -351,11 +351,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-# Disable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
-endif
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Model is set via init library
