63377e031cc5858ecd733f3ca696504914cd985c
Rk779 <Rahulkrishna585@gmail.com>
2018-04-07 14:03:53 +0000

    s2: Add thermal HAL from CAF and switch to qcom thermal engine
    Change-Id: Ib450e4354045cdb6b5dad77f6dbe83409924bfb5

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -82,7 +82,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
