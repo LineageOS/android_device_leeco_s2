e6502478db7ee8c02c851767130a732ca5435073
Rk779 <Rahulkrishna585@gmail.com>
2018-04-07 14:15:40 +0000

    s2: Add vr HAL from CAF
    Change-Id: I3c5fb32dcb3120e70044b3c831c871afadd2b2f1

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -93,6 +93,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# VR
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-impl \
+    android.hardware.vr@1.0-service
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
