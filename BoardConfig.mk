fc9c5aba197f36b2df587b8fceb9be55faa8f0af
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 22:23:13 +0530

    s2: Mount dsp and firmware in vendor
    Change fstab to mount dsp and firmware to /vendor/dsp and
    /vendor/firmware_mnt.
    
    Add classpath for firmware loading /vendor/firmware_mnt/image in
    kernel command line.
    
    Change-Id: I2dd2e7ea6334fb979ad8ff8c755648748de93ab4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk loop.max_part=7
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x00000100
