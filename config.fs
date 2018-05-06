f5e3bfa41983b4fd722fe929de4a0816426a0749
SagarMakhar <sagarmakhar@gmail.com>
2018-05-06 07:16:48 +0000

    s2: move executables to vendor
    Change-Id: I4ab803bc6c4a7a7a1068155f4695d57616717b7e

--- config.fs
+++ config.fs
@@ -1,4 +1,4 @@
-[system/bin/qmuxd]
+[vendor/bin/qmuxd]
 mode: 0700
 user: AID_RADIO
 group: AID_SHELL
@@ -10,31 +10,31 @@ user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
 
-[system/bin/pm-service]
+[vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/cnss-daemon]
+[vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/slim_daemon]
+[vendor/bin/slim_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_GPS
