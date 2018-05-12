85b2474fbf3b5eaf931815c4fe13c4a8cbe2b740
David Ng <dave@codeaurora.org>
2018-05-11 18:15:13 -0700

    s2: config.fs: Specify firmware image files as system:system
    Explicitly specify firmware image files legacy /firmware/image/*
    as system:system and new /vendor/firmware_mnt/image/* as root:system
    
    For normal boot, this is a no-op as /firmware|/vendor/firmware_mnt
    is a mount point.  This change is only for development when
    firmware is read-writable and allows changes via "adb push".  This
    match the underlying FAT fs mount ownership of system:system or
    root:system so adb push's chown will not error out.
    
    Change-Id: I90e7528bcd21cca449cc5637d20af8dc9ccf7d3c

--- config.fs
+++ config.fs
@@ -157,6 +157,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
 [persist/]
 mode: 0771
 user: AID_SYSTEM
