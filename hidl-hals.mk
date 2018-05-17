4eeec7b5de67db6bea91d3fc8bc0aff473b5c464
Himanshu Agrawal <hagraw@codeaurora.org>
2018-05-17 17:24:14 +0530

    s2: Changing healthd version to 2.0
    Change-Id: I13ddaa133bb9edcca200d5fe7320cf5fa9f66f0f

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -69,9 +69,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
