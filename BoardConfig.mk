114e6ed4a7ce853855df91e02d86f6dd1fd7e5f3
SagarMakhar <sagarmakhar@gmail.com>
2018-05-28 08:58:16 +0000

    s2: selinux: switch to enforced selinux
    Change-Id: Iaf7cc08aa88fb0c49746fc938a9a4c5e3d5cd749

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,6 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
