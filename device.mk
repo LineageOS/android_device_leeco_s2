22b3187b519b7cc0e203c3a7428e4c89200f5ae2
Rk779 <Rahulkrishna585@gmail.com>
2018-06-07 11:10:20 +0000

    s2: Convert audio_effects.conf to audio_effects.xml
    * https://github.com/luk1337/aeffects-conf2xml
    
    Change-Id: I8091de6e882e66f8f5d3cc54497d93b98946ce38
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
