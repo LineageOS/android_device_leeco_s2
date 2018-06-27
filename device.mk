58896ba263e8c3f713d4ef30f4d0366581cce4c5
Ethan Chen <intervigil@gmail.com>
2018-06-26 23:26:01 -0700

    s2: Remove hwui properties
    Change-Id: Ife1ab3a43627e70608a105b3237905ec568d18ed

--- device.mk
+++ device.mk
@@ -28,9 +28,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
 
-# Dalvik heap and hwui memory limits
+# Dalvik heap memory limits
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # HIDL HALs
 $(call inherit-product, $(LOCAL_PATH)/hidl-hals.mk)
