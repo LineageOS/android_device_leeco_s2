7eb4e53d718a4c824ac343840f067710aea1c094
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-08-05 13:07:21 +0800

    s2: Add input group to sensors service
    * import default sensors hidl implementation
    * convert to s2 target
    * add input group to .rc file
    * binderize sensor hal
    
    Change-Id: Ifaefa77d194faab93674263ca4d6b144ff46c38c

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service.s2
 
 # Thermal
 PRODUCT_PACKAGES += \
