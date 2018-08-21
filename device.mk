6a14db10ae2505f7c36fbe60e1c670ece8f4a39d
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-21 10:26:32 +0200

    Revert "s2: audio: rename aanc_tuning_mixer to audio_tuning_mixer"
    This reverts commit e06b89d1d794b0e4ad9842a3d2527ce2ed426c02.
    
    Change-Id: I98ac92c769529848eb010dfa788380acda4fffda

--- device.mk
+++ device.mk
@@ -97,7 +97,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
