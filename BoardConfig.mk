37a7782a7b156ec777cb14d7e940576c6f248c78
Bruno Martins <bgcngm@gmail.com>
2018-09-03 00:40:59 +0200

    s2: Adapt for local LineageHW java overlays
    Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,7 +189,7 @@ TARGET_PROVIDES_KEYMASTER := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage Hardware
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
