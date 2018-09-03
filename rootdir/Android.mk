69a7ebc14c6e393cbf186e8155d17959516d1ea8
renjian2 <renjian503@gmail.com>
2018-09-03 12:26:21 +0000

    s2: drop post_boot script and use init.qcom.power.rc
    Change-Id: I75fb764894502b4b5d127c005a070562ad1e1fbf

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,10 +54,9 @@ include $(BUILD_PREBUILT)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.post_boot.sh
+LOCAL_MODULE		:= init.qcom.power.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.post_boot.sh
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
+LOCAL_SRC_FILES		:= etc/init.qcom.power.rc
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
-
