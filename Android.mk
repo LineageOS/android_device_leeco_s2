3fcc4f7a18b030d9e9df40c145155c1d1d0b8e43
Rk779 <Rahulkrishna585@gmail.com>
2018-09-08 14:05:23 +0000

    s2: wifi: move symlinks to vendor
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>
    Change-Id: If443619548aa7622fde7a421d8150cd7b281a346

--- Android.mk
+++ Android.mk
@@ -57,16 +57,16 @@ $(WCNSS_CFG_INI): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS_qcom_cfg.ini firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /data/misc/wifi/$(notdir $@) $@
+	$(hide) ln -sf /data/vendor/wifi/$(notdir $@) $@
 
-WCNSS_DICT_DAT := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+WCNSS_DICT_DAT := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
 $(WCNSS_DICT_DAT): $(LOCAL_INSTALLED_MODULE)
 	@echo "WCNSS_wlan_dictionary.dat firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
 	$(hide) ln -sf /persist/$(notdir $@) $@
 
-WLAN_MAC := $(TARGET_OUT_ETC)/firmware/wlan/prima/wlan_mac.bin
+WLAN_MAC := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/wlan_mac.bin
 $(WLAN_MAC): $(LOCAL_INSTALLED_MODULE)
 	@echo "wlan_mac.bin firmware link: $@"
 	@mkdir -p $(dir $@)
