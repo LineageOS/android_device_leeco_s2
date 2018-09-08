1fa35430836db482994f6a387446f9c4f2c2aaad
Rk779 <Rahulkrishna585@gmail.com>
2018-09-08 19:10:47 +0000

    s2: Use passthrough sensor hal * Sometimes proximity sensor and auto brightness doesnt work. After reverting back to passthrough mode works fine.
    Change-Id: I9037175b7b3dc9808dcfa7b735b16d628ca327f4

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -100,8 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # Thermal
 PRODUCT_PACKAGES += \
