ec4cbd2921e168dbf5e99643c7e3020d286c283f
Vladimir Oltean <olteanv@gmail.com>
2018-09-26 20:10:59 +0300

    s2: vintf: switch hwcomposer and camera HIDL HALs to binderized
    * What do the hwcomposer and camera HALs have in common?
      The libqservice library, part of the legacy qcom display HAL stack.
      This library registers the display.qservice with the service manager,
      which is then also accessed through Binder by the camera HAL.
    * The entity who links with libqservice is actually linking with it
      indirectly, since it is a direct dependency of the legacy hwcomposer.
    * Via the hwc2on1adapter, the process that actually hosts the
      libqservice code is the surfaceflinger.
    * The above is true when the hwcomposer HAL is same-process. This leads
      to the following SELinux denial:
    
      avc: denied { add find } for service=display.qservice scontext=u:r:surfaceflinger:s0
           tcontext=u:object_r:default_android_service:s0 tclass=service_manager
      E VendorServiceManager: add_service('display.qservice',1) uid=1000 - PERMISSION DENIED
    
    * This means that surfaceflinger, via the same-process hwcomposer HAL,
      is trying (and failing) to register display.qservice with the *system*
      service manager (/dev/binder). Note that due to the service being
      loaded by surfaceflinger (a system process which doesn't see
      /dev/vndbinder), even the security context is invalid
      (device/qcom/sepolicy-legacy/common/vndservice_contexts defines it
      as qdisplay_service).
    * Naturally, it is not a good idea to grant the above SELinux permission.
      Instead, by running the hwcomposer HAL in its own vendor daemon
      process, display.qservice can be correctly started as a vendor service
      and visible in $(vndservice list).
    * However, now a new problem arises: the camera HAL (via the camera
      server process) is no longer able to access display.qservice, since
      they belong to different Binder domains. The solution to this is to
      switch the camera HAL to binderized as well.
    
    Based on a discussion with: Łukasz Patron (LuK1337 <priv.luk@gmail.com>)
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Change-Id: I6408ceb44136b4924589536a7a012a6fac68e065

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -29,7 +29,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-external-service
 
 # Configstore
 PRODUCT_PACKAGES += \
@@ -44,6 +46,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
