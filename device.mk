2adab526fe9d1d38d9b31319727358cac869d16f
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-11 04:34:44 +0000

    s2: Use stock EUI offline charging animation
    * Thanks to @CrisBalGreece for extracting the required files from EUI ROM
    
    Change-Id: Ib1d86d76ecc161366bf7c7b7909260732c1d02b4

--- device.mk
+++ device.mk
@@ -155,6 +155,10 @@ endif
 PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
