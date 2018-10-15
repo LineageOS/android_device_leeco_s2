1f0b63ef8b079a3316a5f9799e9d12239c8eab84
dianlujitao <dianlujitao@lineageos.org>
2018-10-15 16:06:26 +0800

    s2: Remove power-off alarm stuff from init extension
     * CAF's init.qcom.early_boot.sh checks /persist/alarm/powerOffAlarmSet
       before setting ro.alarm_boot, but on Oreo nothing writes to that
       file, thus code is never executed and the prop is never set.
     * Without ro.alarm_boot=true, the system will boot normally instead of
       boot to min framework even on rtc, and alarm will ring after boot
       completed.
    
    Change-Id: Ie4d30358314337c229a66a16195f675198ef7d52

--- init/init_s2.cpp
+++ init/init_s2.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
-   Copyright (c) 2017, The LineageOS Project
+   Copyright (c) 2017-2018, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -47,82 +47,19 @@ using android::init::property_set;
 static int read_file2(const char *fname, char *data, int max_size)
 {
     int fd, rc;
-
     if (max_size < 1)
         return 0;
-
     fd = open(fname, O_RDONLY);
     if (fd < 0) {
         return 0;
     }
-
     rc = read(fd, data, max_size - 1);
     if ((rc > 0) && (rc < max_size))
         data[rc] = '\0';
     else
         data[0] = '\0';
     close(fd);
-
     return 1;
-}
-
-void init_alarm_boot_properties()
-{
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[64];
-
-    if(read_file2(alarm_file, buf, sizeof(buf))) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-        if (buf[0] == '0') {
-            property_set("ro.boot.bootreason", "invalid");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '1') {
-            property_set("ro.boot.bootreason", "hard_reset");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '2') {
-            property_set("ro.boot.bootreason", "smpl");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '3'){
-            property_set("ro.alarm_boot", "true");
-        }
-        else if (buf[0] == '4') {
-            property_set("ro.boot.bootreason", "dc_chg");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '5') {
-            property_set("ro.boot.bootreason", "usb_chg");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '6') {
-            property_set("ro.boot.bootreason", "pon1");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '7') {
-            property_set("ro.boot.bootreason", "cblpwr");
-            property_set("ro.alarm_boot", "false");
-        }
-        else if (buf[0] == '8') {
-            property_set("ro.boot.bootreason", "kpdpwr");
-            property_set("ro.alarm_boot", "false");
-        }
-    }
 }
 
 void vendor_load_properties() {
@@ -175,6 +112,4 @@ void vendor_load_properties() {
         // Unknown variant
         property_set("ro.product.model", "X52X");
     }
-
-    init_alarm_boot_properties();
 }
