6112f0084dae0cb5ce32f0e331a024a904aa5a69
Rk779 <Rahulkrishna585@gmail.com>
2018-10-22 13:41:40 +0000

    s2: Go back to passthrough camera hidl hal
    * Fixes lags while switching between rear cam and front cam
    
    Change-Id: I99790b8a444e83a7cf52c075e31a3ee9b6c8188c

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -26,8 +26,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl
 
 # Configstore
 PRODUCT_PACKAGES += \
