df1b10cf326c70740ab76608e8c216008dda3c13
Rk779 <Rahulkrishna585@gmail.com>
2018-11-17 12:36:05 +0000

    s2: Rename biometrics hidl hal
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>
    Change-Id: Ic9798e8c9a9c6af78be8605bfb7736911e6c5d32

--- fingerprint/gdxbiometrics/Android.mk
+++ fingerprint/gdxbiometrics/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service-custom
-LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.rc
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.leeco_s2
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.leeco_s2.rc
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
@@ -24,7 +24,7 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     libhwbinder \
     libkeystore_aidl \
-    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1
 
 include $(BUILD_EXECUTABLE)
 
