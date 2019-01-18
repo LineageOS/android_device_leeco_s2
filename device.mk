5797caee4f6b4c99af28ae1bd4cd2d94af7bfe8f
Paul Keith <javelinanddart@gmail.com>
2019-01-18 23:36:25 +0100

    s2: Build livedisplay 2.0 service
    * Only IPictureAdjustment is added because only that is supported
    
    Change-Id: Ia96dfe341d07c9f79b1ab5bd19af0e32bee4b4ed

--- device.mk
+++ device.mk
@@ -262,10 +262,6 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     libshims_ims
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
