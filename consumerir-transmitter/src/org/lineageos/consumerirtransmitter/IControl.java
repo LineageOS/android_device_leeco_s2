2656f8b5edaad3225762019c74295ec77282246b
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2019-01-20 02:23:20 +0000

    s2: implement binderized consumerir HAL + bridge
    Since LeEco doesn't use any driver blobs for consumerir and instead
    uses a quickset SDK, we need a service that acts as a bridge to the
    quickset SDK for translating calls from our consumerir  HAL,  which
    we named "ConsumerirTransmitter".
    
    How this works is our consumerir HAL receives an IR command,  which
    it  sends  to the ConsumerirTransmitter that translates it  to  the
    quickset SDK.
    
    Change-Id: I9e7eebf7071c06ec1e91cf67171b5aca7ae005d2

---
+++ consumerir-transmitter/src/org/lineageos/consumerirtransmitter/IControl.java
@@ -0,0 +1,80 @@
+package org.lineageos.consumerirtransmitter;
+
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+/**
+ * Service client class. Used to interact with the bound UEI QuickSet service.
+ * Package name doesn't matter, but class must be called "IControl"!!
+ */
+public class IControl {
+    public static final String ACTION = "com.uei.control.IControl";
+
+    /**
+     * This is the package name of the Leeco QuickSet service
+     */
+    public static final String QUICKSET_UEI_PACKAGE_NAME = "com.uei.quicksetsdk.letv";
+
+    /**
+     * This is the service class name within the Quickset Package.
+     */
+    public static final String QUICKSET_UEI_SERVICE_CLASS = "com.uei.control.Service";
+
+    private IBinder _controlService = null;
+
+    public IControl(IBinder service) {
+        this._controlService = service;
+    }
+
+    /**
+     * Get result code for last action from the QuickSet service.
+     *
+     * @return result code, 0 means OK
+     * @throws RemoteException
+     */
+    public int getLastResultcode() throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        try {
+            _data.writeInterfaceToken(ACTION);
+            this._controlService.transact(13, _data, _reply, 0);
+            _reply.readException();
+            return _reply.readInt();
+        } finally {
+            _reply.recycle();
+            _data.recycle();
+        }
+    }
+
+    /**
+     * Transmit custom infrared pattern. Work like the official Android API in
+     * ConsumerIrManager.transmit().
+     *
+     * @param carrierFrequency carrier frequency
+     * @param pattern          IR pattern to send
+     * @return status
+     * @throws RemoteException
+     */
+    public int transmit(int carrierFrequency, int[] pattern) throws RemoteException {
+        Parcel _data = Parcel.obtain();
+        Parcel _reply = Parcel.obtain();
+        int _result = 1;
+        if (this._controlService != null) {
+            try {
+                _data.writeInterfaceToken(ACTION);
+                _data.writeInt(carrierFrequency);
+                _data.writeIntArray(pattern);
+                this._controlService.transact(18, _data, _reply, 0);
+                _reply.readException();
+                _result = _reply.readInt();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                _reply.recycle();
+                _data.recycle();
+            }
+        }
+        return _result;
+    }
+}
