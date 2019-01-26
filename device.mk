5f06d674e443db183e88ade8f70db8a31eb41072
Rk779 <Rahulkrishna585@gmail.com>
2019-01-26 10:39:54 +0000

    s2: Build libmm-omcore and libmm-qcamera from source * remove prebuilt blobs
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>
    Change-Id: I770fcffe1cd659541451c0b87b5bc5d19a937daf

--- device.mk
+++ device.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
     camera.msm8952 \
     libqomx_core \
     libmmcamera_interface \
-    libmmjpeg_interface
+    libmmjpeg_interface \
+    libmm-qcamera
 endif
 
 PRODUCT_COPY_FILES +=  \
@@ -280,6 +281,7 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
+    libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
