3efe67862411d048937c055836fe080b9cdc73a1
Rk779 <Rahulkrishna585@gmail.com>
2019-02-02 17:07:35 +0000

    s2: Fix building of android.hardware.gnss@1.0-service-qti.rc
    Change-Id: I0564a1e9e08015bf428044fa9d7356f76b737f6e

--- gps/android/service.cpp
+++ gps/android/service.cpp
@@ -20,7 +20,7 @@
 
 #define LOG_TAG "android.hardware.gnss@1.1-service-qti"
 
-#include <android/hardware/gnss/1.1/IGnss.h>
+#include <android/hardware/gnss/1.0/IGnss.h>
 #include <hidl/LegacySupport.h>
 
 using android::hardware::gnss::V1_0::IGnss;
