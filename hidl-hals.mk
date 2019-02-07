e92c1a0a5dcac3de065ceed30a79d06aef554f65
Paul Keith <javelinanddart@gmail.com>
2019-02-07 13:12:30 -0600

    s2: Build livedisplay sysfs service
    * Add supported interfaces to manifest while we're at it
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -84,7 +84,8 @@ PRODUCT_PACKAGES += \
 
 # Livedisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Net
 PRODUCT_PACKAGES += \
