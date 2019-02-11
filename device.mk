3aa9444505c1b6c88117923c49f7936192fe941a
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 15:35:46 +0200

    s2: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- device.mk
+++ device.mk
@@ -416,8 +416,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
 # SDcard
 PRODUCT_CHARACTERISTICS := nosdcard
