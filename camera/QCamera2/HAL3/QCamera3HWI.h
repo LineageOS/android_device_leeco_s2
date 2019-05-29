fa1ce6caa238c9fb066ca117bcbb40613de387a8
Kuba <nullbytepl@gmail.com>
2019-05-29 15:38:12 +0530

    s2: camera: Use full resolution while capturing
    It seems like our ov16880 blobs pass wrong picture_min_duration and because of that 16MP isn't reported by #getOutputSizes() but by #getHighResolutionOutputSizes(). This patch makes full res appear in #getOutputSizes().
    
    Change-Id: I4557b75e32be920cf293aba8fe04d1152ea6a476

--- camera/QCamera2/HAL3/QCamera3HWI.h
+++ camera/QCamera2/HAL3/QCamera3HWI.h
@@ -172,6 +172,7 @@ public:
                             nsecs_t timestamp, int32_t request_id,
                             const CameraMetadata& jpegMetadata, uint8_t pipeline_depth,
                             uint8_t capture_intent, uint8_t fwk_cacMode);
+    static void patchCaps();
     int initParameters();
     void deinitParameters();
     QCamera3ReprocessChannel *addOfflineReprocChannel(const reprocess_config_t &config,
