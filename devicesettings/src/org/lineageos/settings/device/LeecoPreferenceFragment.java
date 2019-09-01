9a0ec5e4015ef5e41909f17501135c09f7823e0b
Bruno Martins <bgcngm@gmail.com>
2019-09-01 13:05:37 +0100

    s2: devicesettings: Migrate to androidx
    Change-Id: Ia741c785c3ca1d6fb3d41ac68d64865731fba129

--- devicesettings/src/org/lineageos/settings/device/LeecoPreferenceFragment.java
+++ devicesettings/src/org/lineageos/settings/device/LeecoPreferenceFragment.java
@@ -18,10 +18,10 @@ package org.lineageos.settings.device;
 
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 import android.util.Log;
 import android.widget.Toast;
 
