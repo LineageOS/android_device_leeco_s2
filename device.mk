f064680ce7856eb1c061a588ea2bd78ff7c3fa98
dianlujitao <dianlujitao@lineageos.org>
2019-09-16 17:40:51 +0800

    s2: Create system copy of ims-ext-common and add to classpath
     * Our ims.apk doesn't load ims-ext-common.jar, and it's moved to /product
       on 10 where PRODUCT_BOOT_JARS doesn't work.
     * Create a system copy of the jar and add it to boot classpath to make
       it loadable from the apk without extra work,
    
    Change-Id: Ia530a530bd4469e12500410fc5e8ba393de60b18

--- device.mk
+++ device.mk
@@ -245,10 +245,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/synaptics_dsx_s2.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx_s2.kl \
     $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_rmi4_i2c.kl
 
-# IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common
-
 # IPA Manager
 $(call inherit-product, $(LOCAL_PATH)/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
 
@@ -363,9 +359,11 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common_system \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier smart selection model files
