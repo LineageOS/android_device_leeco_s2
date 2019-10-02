39639b6c8a94e008da7713a517714484841c051b
Steven Moreland <smoreland@google.com>
2019-10-02 12:49:15 -0700

    s2: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: Iba2cd20b8b20fac3e7564de6b853b475fd4ebd2a

--- data-ipa-cfg-mgr/hal/Android.mk
+++ data-ipa-cfg-mgr/hal/Android.mk
@@ -12,9 +12,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
 LOCAL_MODULE := liboffloadhal
 
 #LOCAL_CPP_FLAGS := -Wall -Werror
-LOCAL_SHARED_LIBRARIES := libhwbinder \
-                        libhidlbase \
-                        libhidltransport \
+LOCAL_SHARED_LIBRARIES := libhidlbase \
                         liblog \
                         libcutils \
                         libdl \
