60fff9679d50a98df5f32ad5dbb8cc8dc8bc65e9
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    s2: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # RenderScript
 PRODUCT_PACKAGES += \
