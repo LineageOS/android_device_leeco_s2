ab0d3141e515755fa3d0b67738cb762ac7f372c7
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    s2: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
