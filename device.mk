4b3f5a1d2e0f10b7734d134daedeff261438aaf8
Marc Bourgoin <themard85@gmail.com>
2020-01-01 03:29:52 -0700

    s2: symlink bootdevice for Lineage Recovery
    * otherwise we get error messages that /dev/block/bootdevice/ is missing for all the entries in our fstab
    
    Change-Id: Idb90a6ecaae541e74a9878c1ca93459cc2fdd949

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -336,6 +336,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.rc \
+    init.recovery.qcom.rc \
     init.target.rc \
     init.s2.usb.rc \
     ueventd.qcom.rc
