6afa0d22264373ed1fa246ce932c304cb97f477c
chrmhoffmann <chrmhoffmann@gmail.com>
2020-02-02 12:53:51 +0100

    s2: Drop external camera config
    Change-Id: If6e611bbb7897bfbcdba5e381c62348d8d91373c

--- device.mk
+++ device.mk
@@ -155,9 +155,6 @@ PRODUCT_PACKAGES += \
     libmm-qcamera
 endif
 
-PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
