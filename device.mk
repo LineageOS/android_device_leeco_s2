9a3f6c1abdd6f9f21dc6e6ceb44ffec5c58fa8f3
Erfan Abdi <erfangplus@gmail.com>
2020-07-23 00:38:11 +0430

    s2: Nuke android.hidl.manager@1.0_system
    Change-Id: I60eeb3e1f4850c9fa1359756576ddb657ea20d99

--- device.mk
+++ device.mk
@@ -204,8 +204,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0
 
 # Input configuration
 PRODUCT_COPY_FILES += \
