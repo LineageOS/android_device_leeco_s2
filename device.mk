32bf3449d41522d994ba764435455d114edafc0f
Roker2 <dimaminko2000@gmail.com>
2021-03-31 22:30:21 +0300

    s2: Switch to open source keystore
    * this fixes the fingerprint in applications (for example in bank applications)
    
    Change-Id: Ia00c765da483f6f1ad15d2a8ef81d511a74d6813

--- device.mk
+++ device.mk
@@ -242,7 +242,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    keystore.msm8952
 
 # Libshims
 PRODUCT_PACKAGES += \
